e21f3823f1921af7545e94c0d614af1c0b4fe8c1
Marcin Kołodziej <dev@izoslav.pl>
2020-12-06 02:40:48 +0100

    day 4 and 5

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 mod day1;
 mod day2;
 mod day3;
+mod day4;
+mod day5;
 
 fn main() {
   day1::run();
   day2::run();
   day3::run();
+  day4::run();
+  day5::run();
 
   println!("Hello, world!");
 }
