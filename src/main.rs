e36d09ea87d2259112940278167f3e41b34d9853
Marcin Kołodziej <dev@izoslav.pl>
2020-12-12 13:48:42 +0100

    days 6, 7, & 8

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,19 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
+mod day7;
+mod day8;
 
 fn main() {
-  day1::run();
-  day2::run();
-  day3::run();
-  day4::run();
-  day5::run();
+  println!("Advent of Code 2020 - Rust edition");
 
-  println!("Hello, world!");
+  // day1::run();
+  // day2::run();
+  // day3::run();
+  // day4::run();
+  // day5::run();
+  // day6::run();
+  // day7::run();
+  day8::run();
 }
