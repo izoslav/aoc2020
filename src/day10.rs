19f3db601e033f9a4b008a18bbd3c2ef35329c68
Marcin Kołodziej <dev@izoslav.pl>
2020-12-12 22:39:02 +0100

    day 10 part 2

--- src/day10.rs
+++ src/day10.rs
@@ -1,5 +1,4 @@
 use std::fs;
-use std::cmp;
 
 use itertools::Itertools;
 
@@ -9,6 +8,7 @@ pub fn run() {
 
   println!("=== Day 10 ===");
   println!("1-jolt diffs * 3-jolt diffs: {}", differences.0 * differences.2);
+  println!("Possible combinations: {}", count_combinations(&input))
 }
 
 
@@ -18,61 +18,71 @@ fn load(filepath: &str) -> Vec<u32> {
 
   let contents = String::from(contents);
 
-  contents
+  let mut output = contents
     .trim()
     .split_whitespace()
     .map(|number| number.parse::<u32>().unwrap())
     .sorted()
-    .collect()
+    .collect::<Vec<u32>>();
+
+  output.insert(0, 0);
+  output.push(output[output.len() - 1] + 3);
+
+  output
 }
 
 fn count_joltage_differences(input: &Vec<u32>) -> (u32, u32, u32) {
-  (
-    count_n_differences(&input, 1),
-    count_n_differences(&input, 2),
-    count_n_differences(&input, 3) + 1
-  )
-}
+  let mut diff1 = 0;
+  let mut diff2 = 0;
+  let mut diff3 = 0;
 
-fn count_n_differences(input: &Vec<u32>, difference: u32) -> u32 {
-  let mut sum = if input[0] == difference { 1 } else { 0 };
-  for i in 0..input.len() - 1 {
-    if input[i + 1] - input[i] == difference {
-      sum += 1;
+  for window in input.windows(2) {
+    let diff: i32 = window[1] as i32 - window[0] as i32;
+    match diff {
+      1 => { diff1 += 1 }
+      2 => { diff2 += 1 }
+      3 => { diff3 += 1 }
+      _ => ()
     }
   }
 
-  sum
+  (diff1, diff2, diff3)
 }
 
-fn count_combinations(input: &Vec<u32>, target: u32) -> u64 {
-  count_combinations_step(&input[..], target)
-}
+fn count_combinations(input: &Vec<u32>) -> u64 {
+  let mut slices = vec![];
+  let mut current_slice = vec![];
 
-fn count_combinations_step(input: &[u32], target: u32) -> u64 {
-  // println!("input {:?} target {}", input, target);
-
-  if input[0] == target - 3 {
-    println!("HIT!");
-    return 1;
+  for window in input.windows(2) {
+    match window[1] - window[0] {
+      1 => current_slice.push(window[0]),
+      3 => {
+        current_slice.push(window[0]);
+        slices.push(current_slice);
+        current_slice = vec![];
+      }
+      _ => (),
+    }
   }
 
-  if input.len() == 1 {
-    return 0;
-  }
-
-  let mut acc = 0;
-  for i in 1..cmp::min(3, input.len() - 1) {
-    acc += count_combinations_step(&input[i..], target);
-  }
-  acc
+  slices
+    .iter()
+    .map(|slice| match slice.len() {
+      1 => 1,
+      2 => 1,
+      3 => 2,
+      4 => 4,
+      5 => 7,
+      _ => panic!("unexpected slice of size N > 5 consecutive 1-diff elements"),
+    })
+    .product()
 }
 
 #[test]
 fn test_small_example() {
   let input = load("inputs/day10/example_small.txt");
   println!("{:?}", &input);
-  
+
   let expected_d1 = 7;
   let expected_d3 = 5;
   let differences = count_joltage_differences(&input);
@@ -80,9 +90,8 @@ fn test_small_example() {
   assert_eq!(expected_d1, differences.0);
   assert_eq!(expected_d3, differences.2);
 
-  let target = input[input.len() - 1] + 3;
   let expected_combinations = 8;
-  let combinations = count_combinations(&input, target);
+  let combinations = count_combinations(&input);
 
   assert_eq!(expected_combinations, combinations);
 }
@@ -91,7 +100,7 @@ fn test_small_example() {
 fn test_big_example() {
   let input = load("inputs/day10/example_big.txt");
   println!("{:?}", &input);
-  
+
   let expected_d1 = 22;
   let expected_d3 = 10;
   let differences = count_joltage_differences(&input);
@@ -99,9 +108,8 @@ fn test_big_example() {
   assert_eq!(expected_d1, differences.0);
   assert_eq!(expected_d3, differences.2);
 
-  // let target = input[input.len() - 1] + 3;
-  // let expected_combinations = 19208;
-  // let combinations = count_combinations(&input, target);
+  let expected_combinations = 19208;
+  let combinations = count_combinations(&input);
 
-  // assert_eq!(expected_combinations, combinations);
+  assert_eq!(expected_combinations, combinations);
 }
